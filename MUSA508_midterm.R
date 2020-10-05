3b119315c44d2983ba78d975b86d938d127e230f
mlkornhauser <70709544+mlkornhauser@users.noreply.github.com>
2020-10-05 14:00:59 -0400

    Update MUSA508_midterm.R

--- MUSA508_midterm.R
+++ MUSA508_midterm.R
@@ -1,3 +1,6 @@
+#MUSA 508 Midterm
+#Maddy Kornhauser & Adam Ghazzawi
+#Flymiamibro
 
 ########
 # SETUP
@@ -12,6 +15,7 @@ library(FNN)
 library(grid)
 library(gridExtra)
 library(ggcorrplot)
+library(osmdata)
 
 root.dir = "https://github.com/mlkornhauser/MUSA508_midterm.git"
 
@@ -68,16 +72,236 @@ qBr <- function(df, variable, rnd) {
 
 q5 <- function(variable) {as.factor(ntile(variable, 5))}
 
-#################
-# LOAD DATA WRANGLING
-#################
-#Census data
+nn_function <- function(measureFrom,measureTo,k) {
+  measureFrom_Matrix <- as.matrix(measureFrom)
+  measureTo_Matrix <- as.matrix(measureTo)
+  nn <-   
+    get.knnx(measureTo, measureFrom, k)$nn.dist
+  output <-
+    as.data.frame(nn) %>%
+    rownames_to_column(var = "thisPoint") %>%
+    gather(points, point_distance, V1:ncol(.)) %>%
+    arrange(as.numeric(thisPoint)) %>%
+    group_by(thisPoint) %>%
+    summarize(pointDistance = mean(point_distance)) %>%
+    arrange(as.numeric(thisPoint)) %>% 
+    dplyr::select(-thisPoint) %>%
+    pull()
+  
+  return(output)  
+}
 
-miami <- st_read('/Users/mori_ghazzawi/Documents/GitHub/MUSA508_midterm/data/studentsData.geojson') %>%
-  st_transform('ESRI:102728')
-muni <- st_read('https://opendata.arcgis.com/datasets/5ece0745e24b4617a49f2e098df8117f_0.geojson') %>%
-  st_transform(st_crs(miami)) %>%
-  filter(MUNICID =="01" | MUNICID =="02") 
+############
+# LOAD DATA 
+############
+
+miami <- st_read('data/studentsData.geojson')
+
+miami.sf  <- miami %>% 
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, agr = "constant") %>%
+  st_transform('ESRI:102286')
+
+muni.sf <- st_read('https://opendata.arcgis.com/datasets/5ece0745e24b4617a49f2e098df8117f_0.geojson') %>%
+  st_transform(st_crs(miami.sf)) %>%
+  filter(MUNICID =="01" | MUNICID =="02") %>%
+  st_as_sf()
+
+# I moved other GEOJSON links from the Miami data portal to the end of the script.
+
+#Open Street Map Workflow
+#install.packages("osmdata")
+#https://wiki.openstreetmap.org/wiki/Map_Features
+#Check above link for list of available features
+
+miami.base <- 
+  st_read("https://opendata.arcgis.com/datasets/5ece0745e24b4617a49f2e098df8117f_0.geojson") %>%
+  filter(NAME == "MIAMI BEACH" | NAME == "MIAMI") %>%
+  st_union()
+
+#Setting the bounding box
+xmin = st_bbox(miami.base)[[1]]
+ymin = st_bbox(miami.base)[[2]]
+xmax = st_bbox(miami.base)[[3]]  
+ymax = st_bbox(miami.base)[[4]]
+
+#Loading data from OSM - example set = bars
+bars <- opq(bbox = c(xmin, ymin, xmax, ymax)) %>% 
+  add_osm_feature(key = 'amenity', value = c("bar")) %>%
+  osmdata_sf()
+
+bars <- 
+  bars$osm_points %>%
+  .[miami.base,]
+
+ggplot() +
+  geom_sf(data=miami.base, fill="black") +
+  geom_sf(data=bars, colour="red", size=.5) 
+
+#-----Buffer: Count within buffer of each house
+#Convert OSM data into an sf object with same projection
+bars.sf <-
+  bars %>%
+  dplyr::select(geometry) %>%
+  na.omit() %>%
+  st_transform(st_crs(miami.sf)) %>%
+  distinct()
+
+#Adding column to main dataset indicating the count of X within the buffer area
+miami.sf$bars.buffer_660 = #I think it woudl be good practice to list the number of feet in the column name
+  st_buffer(miami.sf, 660) %>%
+  aggregate(mutate(bars.sf, counter = 1),., sum) %>%
+  pull(counter) #might need to change the NA values to 0
+
+miami.sf$bars.buffer_2640 =
+  st_buffer(miami.sf, 2640) %>%
+  aggregate(mutate(bars.sf, counter = 1),., sum) %>%
+  pull(counter) #might need to change the NA values to 0
+
+#generate a density plot
+ggplot() + geom_sf(data = muni.sf, fill = "grey40") +
+  stat_density2d(data = data.frame(st_coordinates(bars.sf)), 
+                 aes(X, Y, fill = ..level.., alpha = ..level..),
+                 size = 0.01, bins = 40, geom = 'polygon') +
+  scale_fill_gradient(low = "#25CB10", high = "#FA7800", name = "Density") +
+  scale_alpha(range = c(0.00, 0.35), guide = FALSE) +
+  labs(title = "Density of Bars, Miami") +
+  mapTheme()
+
+
+#-----Nearest Neighbor Feature Workflow
+#rename st_coordinates for brevity
+st_c <- st_coordinates
+
+#generate nearest neighbor values in the miami.sf dataset
+miami.sf <-
+  miami.sf %>%
+  mutate(
+    bars_nn1 = nn_function(st_c(st_centroid(miami.sf)), st_c(st_centroid(bars.sf)), 1),
+    bars_nn2 = nn_function(st_c(st_centroid(miami.sf)), st_c(st_centroid(bars.sf)), 2),
+    bars_nn3 = nn_function(st_c(st_centroid(miami.sf)), st_c(st_centroid(bars.sf)), 3),
+    bars_nn4 = nn_function(st_c(st_centroid(miami.sf)), st_c(st_centroid(bars.sf)), 4),)
+
+#plot NN count over space - Should increase or decrease?
+miami.sf.plot <- miami.sf %>%
+  st_drop_geometry() %>%
+  dplyr::select(Folio, starts_with("bars_")) %>%
+  tidyr::pivot_longer(cols = -Folio, names_to = "bars_nn")
+
+ggplot(miami.sf.plot, aes(x = bars_nn, y = value, group = Folio)) +
+  geom_line(alpha = 0.05, color = "firebrick") +
+  theme_bw()
+
+
+######################
+# EXPLORATORY ANALYSIS
+######################
+#Code for corr plots
+st_drop_geometry(miami.sf) %>% 
+  # mutate(Age = 2020 - YearBuilt) %>% #calculating the age of the home
+  # mutate(BR_4more = Bed > 4) %>% #categorical variables for the number of bedrooms in a house.
+  # mutate(BR_4 = Bed == 4) %>%
+  # mutate(BR_3 = Bed == 3) %>%
+  # mutate(BR_2 = Bed == 2) %>%
+  # mutate(BR_1 = Bed == 1) %>%
+  dplyr::select(SalePrice, bars_nn1, bars_nn2, bars_nn3, bars_nn4, bars.buffer_2640) %>% #Choose variables from main dataset
+  gather(Variable, Value, -SalePrice) %>% #convert to long format
+  ggplot(aes(Value, SalePrice)) + #plot
+  geom_point(size = .5) + geom_smooth(method = "lm", se=F, colour = "#FA7800") +
+  facet_wrap(~Variable, ncol = 3, scales = "free") +
+  labs(title = "Price as a function of continuous variables") +
+  plotTheme()
+
+#Corr matrix
+numericVars <- 
+  select_if(st_drop_geometry(miami.sf), is.numeric) %>% na.omit()
+
+ggcorrplot(
+  round(cor(numericVars), 1), 
+  p.mat = cor_pmat(numericVars),
+  colors = c("#25CB10", "white", "#FA7800"),
+  type="lower",
+  insig = "blank") +  
+  labs(title = "Correlation across numeric variables") 
+#This is messy - could drop some variables to make this clearer.
+
+#####################
+# REGRESSION WORKFLOW
+#####################
+#Subset miami dataframe into all homes with listed sale prices
+sales <- subset(miami.sf, SalePrice > 0)
+head(sales)
+
+#Setting up test and training datasets
+inTrain <- createDataPartition( 
+  y = paste(sales$SalePrice), 
+  p = .60, list = FALSE)
+
+miami.training <- sales[inTrain,] 
+miami.test <- sales[-inTrain,]  
+
+#Multivariate regression
+reg1 <- lm(SalePrice ~ ., data = st_drop_geometry(miami.training) %>% 
+             dplyr::select(SalePrice, AdjustedSqFt, LotSize,
+                           Bed, Bath, YearBuilt, bars_nn4))
+summary(reg1)
+
+#Predicting test values using regression
+reg1_predict <- predict(reg1, newdata = miami.test)
+
+#Calculating mean square error
+rmse.train <- caret::MAE(predict(reg1), miami.training$SalePrice)
+rmse.test  <- caret::MAE(reg1_predict, miami.test$SalePrice)
+
+cat("Train MAE: ", as.integer(rmse.train), " \n","Test MAE: ", as.integer(rmse.test))
+
+preds.train <- data.frame(pred   = predict(reg1),
+                          actual = miami.training$SalePrice,
+                          source = "training data")
+preds.test  <- data.frame(pred   = reg1_predict,
+                          actual = miami.test$SalePrice,
+                          source = "testing data")
+preds <- rbind(preds.train, preds.test)
+
+ggplot(preds, aes(x = pred, y = actual, color = source)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "green") +
+  geom_abline(color = "orange") +
+  coord_equal() +
+  theme_bw() +
+  facet_wrap(~source, ncol = 2) +
+  labs(title = "Comparing predictions to actual values",
+       x = "Predicted Value",
+       y = "Actual Value") +
+  theme(
+    legend.position = "none"
+  )
+
+# Cross validation
+fitControl <- trainControl(method = "cv", 
+                           number = 10,
+                           savePredictions = FALSE)
+
+set.seed(717)
+reg1.cv <- 
+  train(SalePrice ~ ., data = st_drop_geometry(sales) %>% 
+          dplyr::select(SalePrice, AdjustedSqFt, LotSize,
+                        Bed, Bath, YearBuilt, bars_nn4), 
+        method = "lm", 
+        trControl = fitControl, 
+        na.action = na.pass)
+
+reg1.cv 
+#this line of code shows the average values across all 10 folds.
+#RMSE = [R] Mean Squared Error
+#MAE = Mean Average Error.  I think this is the value that we're interested in.
+
+reg1.cv$resample
+#This line of code shows the above values for each individual fold
+
+
+######################
+# Online JSON sources
+######################
 water <- st_read('https://opendata.arcgis.com/datasets/b44862ec4a1447c09fc6ff0e3d70f81a_0.geojson') %>%
   st_transform(st_crs(miami))
 
@@ -149,95 +373,7 @@ rail <- st_read('https://opendata.arcgis.com/datasets/ee3e2c45427e4c85b751d8ad57
   st_as_sf()
 rail <- st_join(rail, muni, join = st_intersects, left = FALSE)
 
-#Subset miami dataframe into all homes with listed sale prices
-sales <- subset(miami, SalePrice > 0)
-
-
-inTrain <- caret::createDataPartition( 
-  y = paste(sales$SalePrice), 
-  p = .60, list = FALSE)
-
-miami.training <- sales[inTrain,] 
-miami.test <- sales[-inTrain,]  
-
-#Looking at home characteristics and the relationship to price
-reg1 <- lm(SalePrice ~ ., data = st_drop_geometry(miami.training) %>% 
-             dplyr::select(SalePrice, AdjustedSqFt, LotSize,
-                           Bed, Bath, YearBuilt))
-summary(reg1)
-
-reg1_predict <- predict(reg1, newdata = miami.test)
-
-summary(reg1_predict)
-
-rmse.train <- caret::MAE(predict(reg1), miami.training$SalePrice)
-rmse.test  <- caret::MAE(reg1_predict, miami.test$SalePrice)
-
-preds.train <- data.frame(pred   = predict(reg1),
-                          actual = miami.training$SalePrice,
-                          source = "training data")
-preds.test  <- data.frame(pred   = reg1_predict,
-                          actual = miami.test$SalePrice,
-                          source = "testing data")
-preds <- rbind(preds.train, preds.test)
-
-ggplot(preds, aes(x = actual, y = pred, color = source)) +
-  geom_point() +
-  geom_smooth(method = "lm", color = "green") +
-  geom_abline(color = "orange") +
-  coord_equal() +
-  theme_bw() +
-  facet_wrap(~source, ncol = 2) +
-  labs(title = "Comparing predictions to actual values",
-       x = "Predicted Value",
-       y = "Actual Value") +
-  theme(
-    legend.position = "none"
-  )
-
-#Home Buffers
-homebuffers <- 
-  miami %>%
-  st_buffer(2640) %>%
-  dplyr::select(Folio)
-
-j <- 
-  st_join(PO, homebuffers) %>%
-  filter(!is.na(Folio))
-
-PObyHome <-
-  j %>%
-  group_by(Folio) %>%
-  summarise(PO_Count = n())
-
-clip_PO <- 
-  st_intersection(homebuffers, PO) %>%
-  mutate(Selection_Type = "Clip") %>%
-  summarise(Total_Count = n())
-
-
 #Looking at types ofbusinesses
 commercial_group <- commercial %>% count(BUSDESC)
 commercial_group <- commercial_group[order(-commercial_group$n),]
 commercial_group
-
-#need crime data
-#need neighborhoods for miami beach
-
-View(miami)
-head(miami)
-
-ggplot() + 
-  geom_sf(data = muni) +
-  geom_sf(data = miami, aes(colour = q5(SalePrice))) 
-
-head(water)
-
-ggplot() +
-  geom_sf(data = muni, fill = "grey40") +
-  geom_sf(data = tracts, fill = "transparent") +
-  geom_sf(data = miami, aes(colour = q5(SalePrice)))
-  
-
-
-
